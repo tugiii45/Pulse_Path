01a23aa038fb817a900c200892c7432ba71dae95
Conrad <conradkaranja5@gmail.com>
2026-07-21 11:34:38 +0300

    feat(accounts) : add patient model and serializer

--- accounts/serializers/__init__.py
+++ accounts/serializers/__init__.py
@@ -1,1 +1,2 @@
 from .auth import RegisterSerializer, ProfileSerializer
+from .patient import PatientSerializer
---
+++ accounts/serializers/patient.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from accounts.models import Patient
+
+class PatientSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Patient
+        fields = ('id', 'full_name', 'email', 'date_of_birth', 'gender', 'blood_group', 'emergency_contact', 'address', 'created_at')
+        read_only_fields = ["id", "created_at"]
+
+        def get_full_name(self, obj):
+            return obj.user.get_full_name()
+
+        def get_email(self, obj):
+            return obj.user.email
