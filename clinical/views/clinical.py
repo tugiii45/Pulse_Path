409b085329d289eaaa8782f79351895366ccf5a6
Conrad <conradkaranja5@gmail.com>
2026-07-28 11:38:25 +0300

    Add role-based permissions to clinical app

--- clinical/views/clinical.py
+++ clinical/views/clinical.py
@@ -3,13 +3,14 @@ from rest_framework.permissions import IsAuthenticated
 
 from ..models import ClinicalRecord
 from ..serializers.clinical_record import ClinicalRecordSerializer
+from accounts.permissions import *
 
 class ClinicalRecordListCreateView(generics.ListCreateAPIView):
     queryset = ClinicalRecord.objects.all()
     serializer_class = ClinicalRecordSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsDoctorOrAdmin]
 
 class ClinicalRecordDetailView(generics.RetrieveUpdateDestroyAPIView):    
     queryset = ClinicalRecord.objects.all()
     serializer_class = ClinicalRecordSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsDoctorOrAdmin]
--- clinical/views/diagnosis.py
+++ clinical/views/diagnosis.py
@@ -2,13 +2,14 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from ..models import Diagnosis
 from ..serializers.diagnosis import DiagnosisSerializer
+from accounts.permissions import *
 
 class DiagnosisListCreateView(generics.ListCreateAPIView):
     queryset = Diagnosis.objects.all()
     serializer_class = DiagnosisSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsDoctorOrAdmin]
 
 class DiagnosisDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Diagnosis.objects.all()
     serializer_class = DiagnosisSerializer
-    permission_classes = [IsAuthenticated]    
+    permission_classes = [IsAuthenticated, IsDoctorOrAdmin]    
