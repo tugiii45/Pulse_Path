9bcc6303580f94c3641237550e0c03eb4b327d75
Conrad <conradkaranja5@gmail.com>
2026-07-29 12:57:06 +0300

    Add filtering, search and ordering to Appointment API

--- visits/views/appointment.py
+++ visits/views/appointment.py
@@ -1,13 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated
 from ..models import Appointment
 from ..serializers import AppointmentSerializer
 from accounts.permissions import *
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class AppointmentListCreateView(generics.ListCreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     permission_classes = [IsAuthenticated, IsDoctorOrAdminOrPatientOwner]
+
+    filter_backends=[DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ["patient", "doctor", "status", ]
+    search_fields = ["patient__user__first_name", "patient__user__last_name", "doctor__user__first_name", "doctor__user__last_name",]
+    ordering_fields = ["appointment_date", "created_at", ]
+    ordering = ["-appointment_date"]
 
 
 class AppointmentDetailView(generics.RetrieveUpdateDestroyAPIView):
