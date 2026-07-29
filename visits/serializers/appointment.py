6f41a70d267e4afe7aa7c7a89339d71c70e2f9c3
Conrad <conradkaranja5@gmail.com>
2026-07-29 16:08:04 +0300

    Add appointment business rules and validation

--- visits/serializers/appointment.py
+++ visits/serializers/appointment.py
@@ -1,9 +1,54 @@
 from rest_framework import serializers
 from ..models import Appointment
+from django.utils import timezone
 
 class AppointmentSerializer(serializers.ModelSerializer):
     patient_name = serializers.CharField(source="patient.user.get_full_name", read_only=True)
     doctor_name = serializers.CharField(source="doctor.user.get_full_name", read_only=True)
+
+    def validate_appointment_date(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError(
+            "Appointment date cannot be in the past."
+        )
+        return value
+
+    def validate(self, attrs):
+      patient = attrs.get("patient")
+      doctor = attrs.get("doctor")
+      appointment_date = attrs.get("appointment_date")
+
+      queryset = Appointment.objects.all()
+
+    # Ignore current object during updates
+      if self.instance:
+        queryset = queryset.exclude(pk=self.instance.pk)
+
+      if queryset.filter(
+        patient=patient,
+        appointment_date=appointment_date
+      ).exists():
+        raise serializers.ValidationError({
+            "patient": "The patient already has an appointment at this time."
+        })
+
+      if queryset.filter(
+        doctor=doctor,
+        appointment_date=appointment_date
+      ).exists():
+        raise serializers.ValidationError({
+         "doctor": "The doctor already has an appointment at this time."
+    })
+
+      appointment_time = appointment_date.time()
+
+      if appointment_time.hour < 8 or appointment_time.hour >= 17:
+       raise serializers.ValidationError({
+        "appointment_date":
+        "Appointments can only be booked between 8:00 AM and 5:00 PM."
+    })
+
+      return attrs
 
     class Meta:
         model = Appointment
