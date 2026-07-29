3969fc76d3e5b3fdde5ef26321ed5e57b7ba8bd4
Conrad <conradkaranja5@gmail.com>
2026-07-29 13:32:36 +0300

    Add notification filtering and fix notification schema

--- notifications/models/notification.py
+++ notifications/models/notification.py
@@ -20,6 +20,6 @@ class Notification(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)  
 
     def __str__(self):
-        return f"{self.title} - {self.user.email}"
+        return f"{self.title} - {self.recipient.email}"
 
     
--- notifications/views/notification.py
+++ notifications/views/notification.py
@@ -1,28 +1,38 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated
 from ..models import *
 from ..serializers import *
 from accounts.permissions import *
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
 class NotificationListCreateView(generics.ListCreateAPIView):
+    queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
+
+    filter_backends=[DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["notification_type", "is_read", "recipient", "created_by",]
+    search_fields = ["title", "message",]
+    ordering_fields = ["created_at",]
+    ordering = ["-created_at"]
  
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
     
 
     def get_queryset(self):
-        user = self.request.user
+      if getattr(self, "swagger_fake_view", False):
+        return Notification.objects.none()
 
-        if user.role == "ADMIN":
-            return Notification.objects.all()
+      user = self.request.user
 
-        if user.role == "DOCTOR":
-            return Notification.objects.filter(created_by=user)
+      if user.role == "ADMIN":
+        return Notification.objects.all()
 
-        return Notification.objects.filter(recipient=user)
+      if user.role == "DOCTOR":
+        return Notification.objects.filter(created_by=user)
 
+      return Notification.objects.filter(recipient=user)
     permission_classes = [IsOwnerOrDoctor]
 
 
