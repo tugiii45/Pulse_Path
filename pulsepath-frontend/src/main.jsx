5076da4e8f90db4faadfe6015d960bb41e681c11
Conrad <conradkaranja5@gmail.com>
2026-08-04 16:13:00 +0300

    Clean Vite starter files and configure Bootstrap

--- pulsepath-frontend/src/main.jsx
+++ pulsepath-frontend/src/main.jsx
@@ -1,7 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
